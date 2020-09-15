452c90e92bb21dd1a4d2c2d7bcfcb45882e6f055
cielpy <beijiu572@gmail.com>
2020-09-15 16:49:45 +0800

    add

---
+++ tic-tac-toe/precache-manifest.784721a95da0a6bd7ae3ee97a5d2b66a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc2b4c622576c59af2313b4b9b49a4e6",
+    "url": "/reactjs-study/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "0c0eac34bcb62183745c",
+    "url": "/reactjs-study/tic-tac-toe/static/css/main.3fa7586b.chunk.css"
+  },
+  {
+    "revision": "02e2e52a12c973697374",
+    "url": "/reactjs-study/tic-tac-toe/static/js/2.d9f45bbd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactjs-study/tic-tac-toe/static/js/2.d9f45bbd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c0eac34bcb62183745c",
+    "url": "/reactjs-study/tic-tac-toe/static/js/main.fcfe7dfe.chunk.js"
+  },
+  {
+    "revision": "6fc667b943204de2a2f3",
+    "url": "/reactjs-study/tic-tac-toe/static/js/runtime-main.f6315927.js"
+  }
+]);
