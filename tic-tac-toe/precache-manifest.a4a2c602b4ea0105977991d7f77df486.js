802dc6ce896cf73ea571751599fe3ec084ce204f
cielpy <beijiu572@gmail.com>
2020-09-15 16:40:09 +0800

    add

---
+++ tic-tac-toe/precache-manifest.a4a2c602b4ea0105977991d7f77df486.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3214219a199751a0401bd82418e9fa4c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2fa25cc4f370c29e81eb",
+    "url": "/static/css/main.3fa7586b.chunk.css"
+  },
+  {
+    "revision": "02e2e52a12c973697374",
+    "url": "/static/js/2.d9f45bbd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d9f45bbd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2fa25cc4f370c29e81eb",
+    "url": "/static/js/main.3daba33b.chunk.js"
+  },
+  {
+    "revision": "7e4128cafe84ec5d966f",
+    "url": "/static/js/runtime-main.ac90f802.js"
+  }
+]);
